0c9e8a820766885daf7298f008a4b47c361ce9bf
dusasrinivas <srinivasdsri1989@gmail.com>
2022-09-17 17:21:05 +0000

    get parameters are not working returns error

--- app.js
+++ app.js
@@ -27,14 +27,95 @@ const initializeDbAndServer = async () => {
 initializeDbAndServer();
 
 app.get("/todos/", async (request, response) => {
-  const { status = "", priority = "" } = request.query;
+  const { status = "", priority = "", search_q = "" } = request.query;
   const statusGetQuery = `
     SELECT * FROM todo
     WHERE 
-    status LIKE '%${status}%',
-    priority LIKE '%${priority}%';`;
+    status LIKE '%${status}%'
+    priority LIKE '%${priority}%'
+    todo LIKE '%${search_q}%'
+    ;`;
   const arrayOf = await db.all(statusGetQuery);
   response.send(arrayOf);
+});
+
+app.get("/todos/:todoId/", async (request, response) => {
+  const { todoId } = request.params;
+  const getToDoQuery = `
+    SELECT
+      *
+    FROM
+      todo
+      WHERE id= ${todoId};`;
+
+  const oneTodo = await db.get(getToDoQuery);
+  response.send(oneTodo);
+});
+
+app.post("/todos/", async (request, response) => {
+  const { id, todo, priority, status } = request.body;
+  const postTodoQuery = `
+  INSERT INTO
+    todo ( id, todo, priority, status)
+  VALUES
+    (${id}, '${todo}', '${priority}', '${status}');`;
+
+  await db.run(postTodoQuery);
+  response.send("Todo Successfully Added");
+});
+
+app.put("/todos/:todoId/", async (request, response) => {
+  const { todoId } = request.params;
+  const requestBody = request.body;
+  let updateColumn = "";
+
+  const getToDo = `
+  SELECT * FROM todo
+  WHERE id=${todoId};`;
+  const previousObject = await db.get(getToDo);
+
+  const {
+    status = previousObject.status,
+    priority = previousObject.priority,
+    todo = previousObject.todo,
+  } = request.body;
+
+  switch (true) {
+    case requestBody.status !== undefined:
+      updateColumn = "Status";
+      break;
+    case requestBody.todo !== undefined:
+      updateColumn = "Todo";
+      break;
+    case requestBody.priority !== undefined:
+      updateColumn = "Priority";
+      break;
+  }
+
+  const putTodo = `
+            UPDATE
+              todo
+            SET
+              priority = '${priority}',
+              status = '${status}',
+              todo = '${todo}'
+              
+            WHERE
+              id = ${todoId};`;
+
+  await db.run(putTodo);
+  response.send(`${updateColumn} Updated`);
+});
+
+app.delete("/todos/:todoId/", async (request, response) => {
+  const { todoId } = request.params;
+  const deleteTodoQuery = `
+  DELETE FROM
+    todo
+  WHERE
+    id = ${todoId};`;
+  await db.run(deleteTodoQuery);
+  response.send("Todo Deleted");
 });
 
 module.exports = app;
